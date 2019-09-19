31d058be380c9eb113e0d98bfde5899720666dc6
Angel Ulises <angelul@outlook.com>
2019-09-19 13:32:53 -0500

    Inclusión de método api/redmine/solicitudes-pendientes

--- vue-mesa-de-ayuda/src/components/historialSolicitudesConsultor.vue
+++ vue-mesa-de-ayuda/src/components/historialSolicitudesConsultor.vue
@@ -81,77 +81,92 @@
 
 <script>
 export default {
-    data(){
-        return {  solicitudes:[
-            {
-                id:1,
-                asunto:"asunto 1",
-                estado: "cerrada",
-                agencia:{
-                    nombre:"agencia 1",
-                     contacto:[
-                        {nombre: "contacto 1.1",
-                        cargo:"Gerente 1.1",
-                        email: "email.example1.1@algo.com",
-                        telefono: "55678907345"},
-                        {nombre: "contacto 1.2",
-                        cargo:"Gerente 1.2",
-                        email: "email.example1.2@algo.com",
-                        telefono: "55678907345"}
-                    ]
-                },
-                modulo:{
-                    id:1,
-                    categoria:"CRM",
-                    modulo: "Filtros"
-                },
-                descripcion:"1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam iaculis egestas ligula, id mattis sem vehicula ac. Quisque a mollis lorem. Cras dignissim in ante non tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc consequat tellus a lectus gravida lacinia. Nunc nulla leo, pulvinar vel ipsum at, efficitur varius nisi. Nullam rutrum urna in mi rhoncus, eu mattis lorem fringilla.",
-                email:"contacto1@general",
-                fecha:"20190917T04:14:11",
-                noOrden: "12345",
-                noPlacas:"12345",
-                noCita:"12345",
-                evidencia:[{
-
-                }]
-            },{
-                id:2,
-                asunto:"asunto 2",
-                estado: "aceptada",
-                agencia:{
-                    nombre:"agencia 2",
-                    contacto:[
-                        {nombre: "contacto 2.1",
-                        cargo:"Gerente 2.1",
-                        email: "email.example2.1@algo.com",
-                        telefono: "55678907345"},
-                        {nombre: "contacto 2.2",
-                        cargo:"Gerente 2.2",
-                        email: "email.example2.2@algo.com",
-                        telefono: "55678907345"}
-                    ]
-                    
-                },
-                modulo:{
-                    id:1,
-                    categoria:"Tableros",
-                    modulo: "Chips"
-                },
-                descripcion:"2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam iaculis egestas ligula, id mattis sem vehicula ac. Quisque a mollis lorem. Cras dignissim in ante non tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc consequat tellus a lectus gravida lacinia. Nunc nulla leo, pulvinar vel ipsum at, efficitur varius nisi. Nullam rutrum urna in mi rhoncus, eu mattis lorem fringilla.",
-                email:"contacto2@general",
-                fecha:"20190917T04:14:11",
-                noOrden: "54321",
-                noPlacas:"54321",
-                noCita:"54321",
-                evidencia:[{
-
-                }]
-            }]
-            };
-    }
-}
+  data() {
+    return {
+      solicitudes: [
+        {
+          id: 1,
+          asunto: "asunto 1",
+          estado: "cerrada",
+          agencia: {
+            nombre: "agencia 1",
+            contacto: [
+              {
+                nombre: "contacto 1.1",
+                cargo: "Gerente 1.1",
+                email: "email.example1.1@algo.com",
+                telefono: "55678907345"
+              },
+              {
+                nombre: "contacto 1.2",
+                cargo: "Gerente 1.2",
+                email: "email.example1.2@algo.com",
+                telefono: "55678907345"
+              }
+            ]
+          },
+          modulo: {
+            id: 1,
+            categoria: "CRM",
+            modulo: "Filtros"
+          },
+          descripcion:
+            "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam iaculis egestas ligula, id mattis sem vehicula ac. Quisque a mollis lorem. Cras dignissim in ante non tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc consequat tellus a lectus gravida lacinia. Nunc nulla leo, pulvinar vel ipsum at, efficitur varius nisi. Nullam rutrum urna in mi rhoncus, eu mattis lorem fringilla.",
+          email: "contacto1@general",
+          fecha: "20190917T04:14:11",
+          noOrden: "12345",
+          noPlacas: "12345",
+          noCita: "12345",
+          evidencia: [{}]
+        },
+        {
+          id: 2,
+          asunto: "asunto 2",
+          estado: "aceptada",
+          agencia: {
+            nombre: "agencia 2",
+            contacto: [
+              {
+                nombre: "contacto 2.1",
+                cargo: "Gerente 2.1",
+                email: "email.example2.1@algo.com",
+                telefono: "55678907345"
+              },
+              {
+                nombre: "contacto 2.2",
+                cargo: "Gerente 2.2",
+                email: "email.example2.2@algo.com",
+                telefono: "55678907345"
+              }
+            ]
+          },
+          modulo: {
+            id: 1,
+            categoria: "Tableros",
+            modulo: "Chips"
+          },
+          descripcion:
+            "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam iaculis egestas ligula, id mattis sem vehicula ac. Quisque a mollis lorem. Cras dignissim in ante non tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc consequat tellus a lectus gravida lacinia. Nunc nulla leo, pulvinar vel ipsum at, efficitur varius nisi. Nullam rutrum urna in mi rhoncus, eu mattis lorem fringilla.",
+          email: "contacto2@general",
+          fecha: "20190917T04:14:11",
+          noOrden: "54321",
+          noPlacas: "54321",
+          noCita: "54321",
+          evidencia: [{}]
+        }
+      ]
+    };
+  },
+  mounted() {
+    // this.$store.commit("isLoaderShown", true); // Esta cosa hace que se muestre el "Cargando..."
+    // this.$http.get("api/redmine/solicitudes-pendientes").then(respuesta => 
+    // {
+    //   this.solicitudes = respuesta.body
+    // })
+    // this.$store.commit("isLoaderShown", false);
+  }
+};
 </script>
 
 <style>
-
 </style>
